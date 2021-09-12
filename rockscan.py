b39d2a706f9861e2040ed60ee1ee7c98f172ac96
Rock-Exploit <85040222+Rock-Exploit@users.noreply.github.com>
2021-09-12 05:59:12 +0300

    Update rockscan.py

--- rockscan.py
+++ rockscan.py
@@ -14,14 +14,6 @@ if nm ==1:
       domin=input("Enter domin:")
       os.system("dmitry -w -s -e " + domin)
 elif nm == 2:
-    print("1-network scan")
-    print("2-port scan")
-    print("3-system scan")
-scan=int(input("Enter number:"))
-ip=input("Enter your ip :")
-if scan ==1:
-      os.system("nmap -sn " + ip)
-elif scan ==2:
-        os.system("nmap -sV -Pn " + ip)
-elif scan ==3:
-        os.system("sudo nmap -sS -sV -Pn --script vuln " + ip)
+      ip=input("Enter your ip :")
+        os.system("sudo nmap  -sS -sV -Pn -A --script vuln " + ip)
+ 
